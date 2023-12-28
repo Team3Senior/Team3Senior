306d94e7f5d7140c157f8451a46f00f3c55e8010
farahkharbech <farah.kh@hotmail.fr>
2023-12-28 16:48:40 +0100

    farah first push

--- client/team3/app/page.tsx
+++ client/team3/app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer/page";
 import Home from "./Home/page";
+import Login from "./Login/page"
 export default function HomePage() {
   return (
     <main >
-      <Home/>
-      <Footer/>
+      {/* <Home/>
+      <Footer/> */}
+      <Login/>
     </main>
   )
 }
