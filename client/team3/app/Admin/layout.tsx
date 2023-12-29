f062c3190beba853b33292d2794efb4436e15fb2
farahkharbech <farah.kh@hotmail.fr>
2023-12-29 14:12:07 +0100

    farah admin interface

---
+++ client/team3/app/Admin/layout.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import next from "next"
+import Link from "next/link"
+export default function AdminLayout({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section>
+        {/* Include shared UI here e.g. a header or sidebar */}
+        <nav >
+        
+        <div className='flex justify-center align-middle bg-black text-white h-20 gap-40 mb-6 items-center' >
+         
+          <Link href='/' >Administration</Link>
+        <Link href='/AdminClients'>Clients</Link>
+        <Link href='/AdminSellers'>Sellers</Link>
+        <Link href='Admin/Categories'>Categories</Link>
+        <Link href='/AdminProducts'>Products</Link>
+        <div>
+        <input type="search"
+            placeholder='What are you looking for?'
+            className='bg-gray-200 p-2 text-xs rounded w-56 h-9 right-36'/>
+              {/* <IoSearchOutline size={25} className='absolute right-10 top-7'/> */}
+        </div>
+        </div>
+       </nav>
+{children}
+      </section>
+    )
+  }
--- client/team3/app/page.tsx
+++ client/team3/app/page.tsx
@@ -1,12 +1,16 @@
 import Footer from "./Footer/page";
 import Home from "./Home/page";
 import Login from "./Login/page"
+import Admin from './Admin/page'
+
 export default function HomePage() {
   return (
     <main >
       {/* <Home/>
       <Footer/> */}
-      <Login/>
+      {/* <Login/> */}
+      {/* <Admin/> */}
+      <h1>hello</h1>
     </main>
   )
 }
