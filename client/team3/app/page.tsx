63bfad241dcd7a4cff223385adb35c6c3bb6f6e5
waelbedoui <waelbedoui10@gmail.com>
2023-12-31 17:29:49 +0100

    bestsellingproduct

--- client/team3/app/page.tsx
+++ client/team3/app/page.tsx
@@ -1,11 +1,12 @@
 
 import Home from "./Home/page";
 import Product from "./Product/page";
+import BestSellingProducts from "./BestSellingProducts/page"
 export default function HomePage() {
   return (
     <main >
-     
-      <Home/>
+     <BestSellingProducts/>
+      
     </main>
   )
 }
